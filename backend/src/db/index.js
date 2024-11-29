8de97117e320d0807bcfde11821dd21be396a7ad
beam-bence <bence@beam.live>
2024-11-29 11:17:02 +0100

    feat(db): addEmail(), searchEmail()

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,4 +1,6 @@
-const knex = require("knex")({
+import knex from "knex";
+
+const db = knex({
   client: "sqlite3", // or 'better-sqlite3'
   connection: {
     filename: "./db.sqlite",
@@ -6,7 +8,21 @@ const knex = require("knex")({
 });
 
 class DB {
+  //TODO create leads schema
   static async addLead(data) {
-    return knex("leads").insert(data);
+    return db("leads").insert(data);
+  }
+  static async addEmail(data) {
+    return db("emails").insert(data);
+  }
+  static async searchEmail(term) {
+    return db("emails")
+      .where("to", "like", `%${term}%`)
+      .orWhere("cc", "like", `%${term}%`)
+      .orWhere("bcc", "like", `%${term}%`)
+      .orWhere("subject", "like", `%${term}%`)
+      .orWhere("body", "like", `%${term}%`);
   }
 }
+
+export default DB;
