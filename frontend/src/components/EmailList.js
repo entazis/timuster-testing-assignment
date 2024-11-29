1aefd04024564e94d73d1e953ad6fc54c1c5b74e
beam-bence <bence@beam.live>
2024-11-29 10:39:30 +0100

    feat(component): EmailItem

--- frontend/src/components/EmailList.js
+++ frontend/src/components/EmailList.js
@@ -1,11 +1,14 @@
 import { List, ListItem } from "@mui/material";
+import EmailItem from "@/components/EmailItem";
 
 export default function EmailList({ emails }) {
   return (
     <List>
       <div>
         {emails.map((email) => (
-          <ListItem key={email.id}>Email</ListItem>
+          <ListItem key={email.id}>
+            <EmailItem email={email} />
+          </ListItem>
         ))}
       </div>
     </List>
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,7 +3,10 @@ import Layout from "@/components/Layout";
 import EmailList from "../components/EmailList";
 
 export default function Home() {
-  const [emails, setEmails] = React.useState([]);
+  const [emails, setEmails] = React.useState([
+    { subject: "test-title-1" },
+    { subject: "test-title-2" },
+  ]);
 
   return (
     <Layout>
