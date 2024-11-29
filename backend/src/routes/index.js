8a31da16c6ec4fe24fc4d0c6564401819d8134b0
beam-bence <bence@beam.live>
2024-11-29 11:19:16 +0100

    feat(routes): /search and /email controllers

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,17 @@
+import DB from "../db/index.js";
+
 export default async function routes(fastify, options) {
   fastify.get("/ping", async (request, reply) => {
     return "pong\n";
+  });
+  fastify.get("/search", async (request, reply) => {
+    const term = request.query?.term || "";
+    return DB.searchEmail(term);
+  });
+  fastify.post("/email", async (request, reply) => {
+    const email = request.body;
+    console.debug("sending email: ", email);
+    const id = await DB.addEmail(email);
+    return { id };
   });
 }
