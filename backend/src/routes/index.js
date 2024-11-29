8eee48cd64525efa137791e919cccf00e42037fd
beam-bence <bence@beam.live>
2024-11-29 11:15:59 +0100

    style(backend): prettier reformat

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,5 +1,5 @@
 export default async function routes(fastify, options) {
-  fastify.get('/ping', async (request, reply) => {
-    return 'pong\n';
+  fastify.get("/ping", async (request, reply) => {
+    return "pong\n";
   });
 }
